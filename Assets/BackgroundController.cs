6876baa6eeb096b9e43ec840fb871b5e75b13aba
neko34p <neko34p@gmail.com>
2018-07-23 22:18:01 +0900

    GoForwardUnityChanプロジェクト　初回コミット
    Lesson7、課題前のソースです。

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+    private float scrollSpeed = -0.03f;
+    private float deadLine = -16;
+    private float startLine = 15.8f;
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //背景を動かす
+        transform.Translate(this.scrollSpeed, 0, 0);
+        //画面外に出たら、画面右端に移動する
+        if(transform.position.x < this.deadLine)
+        {
+            transform.position = new Vector2(this.startLine, 0);
+        }
+		
+	}
+}
